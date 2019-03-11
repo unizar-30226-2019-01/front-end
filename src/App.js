2dd958046e59c67070fa805269974b13c1c6887a
xstarainx <32374903+xstarainx@users.noreply.github.com>
2019-03-11 18:55:29 +0100

    first commit
    Primera prueba componente navbar

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar/>
+        <header className="App-header">
+          
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+
+class Navbar extends Component {
+
+  render() {
+    return(
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <img src={logo}  alt="" width="50" height="60"></img>
+        <a className="navbar-brand" href=".">  Baitu</a>
+        
+        <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+          </ul>
+            
+        </div>
+        <button className="btn btn-outline-primary mr-sm-2" href="#">Registrarse</button>
+        <button className="btn btn-outline-primary" href="#">Iniciar sesión</button>    
+      </nav>
+    )
+  }
+}
+
+export default Navbar
