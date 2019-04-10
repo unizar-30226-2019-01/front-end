b4909d687829499774cb5468aa5246683cbfec85
AlexGuti14 <a.guti3@hotmail.com>
2019-04-11 00:48:18 +0200

    cc

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import App from './App';
 import NavLog from './components/NavLog';
 import NavLogReg from './components/NavLogReg';
 import Registro from './components/Registro';
+import SubirProducto from './components/SubirProducto';
 
 const AppRoutes = () =>
   <div>
@@ -16,6 +17,7 @@ const AppRoutes = () =>
     </Switch>
     <Switch>
       <Route exact path="/registro" component={Registro} />
+      <Route exact path="/subirProducto" component={SubirProducto} />
       <Route exact path="/" component={App} />
     </Switch>
   </div>;
